12345b11fdbf055a624117335e81fcc0b2ac775f
daigoron-sword <dsr.daichi@gamil.com>
2021-04-08 23:19:46 +0900

    Create Mygration File

--- database/migrations/2021_04_08_095222_create_extra_prices_table.php
+++ database/migrations/2021_04_08_095222_create_extra_prices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoomsTable extends Migration
+class CreateExtraPricesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateRoomsTable extends Migration
      */
     public function up()
     {
-        Schema::create('rooms', function (Blueprint $table) {
+        Schema::create('extra_prices', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->integer('price');
-            $table->integer('capacity');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateRoomsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rooms');
+        Schema::dropIfExists('extra_prices');
     }
 }
--- database/migrations/2021_04_08_120426_create_types_table.php
+++ database/migrations/2021_04_08_120426_create_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoomsTable extends Migration
+class CreateTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateRoomsTable extends Migration
      */
     public function up()
     {
-        Schema::create('rooms', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('type');
             $table->integer('price');
-            $table->integer('capacity');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateRoomsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rooms');
+        Schema::dropIfExists('types');
     }
 }
