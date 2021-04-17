a3ea6271048302a55986eea75ae78a1f8b0cbe7c
daigoron-sword <dsr.daichi@gamil.com>
2021-04-17 17:35:47 +0900

    Create Seeder Files

---
+++ database/seeds/ExtraPricesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ExtraPricesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
