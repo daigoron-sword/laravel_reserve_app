bb7d421663f6c33e682561aeb04f271d6dec1bce
daigoron-sword <dsr.daichi@gamil.com>
2021-04-19 18:29:55 +0900

    fix rooms.blade.php

--- resources/views/reserve/rooms.blade.php
+++ resources/views/reserve/rooms.blade.php
@@ -1,9 +1,21 @@
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<title>test</title>
+</head>
+<body>
 <div class="register-input-container">
     <div class="horizontal">
         <div>
             <label for="register-birthday">出身地</label>
         </div>
+        
         <div>
+        
         </div>
     </div>
 </div>
+</body>
+</html>
