267030a732b3c128a66c8fefa97284409ec545a0
daigoron-sword <dsr.daichi@gamil.com>
2021-04-19 23:02:20 +0900

    edit rooms.blade.php

--- resources/views/reserve/rooms.blade.php
+++ resources/views/reserve/rooms.blade.php
@@ -6,16 +6,28 @@
 <title>test</title>
 </head>
 <body>
-<div class="register-input-container">
-    <div class="horizontal">
-        <div>
-            <label for="register-birthday">出身地</label>
-        </div>
-        
-        <div>
-        
+    <div class="container">
+        <div class="row justify-content-left">
+            <div class="col-md-12">
+                {{ Form::open(['url' => '/reserve/meals_plans', 'files' => false]) }}
+                {{ Form::token() }}
+                    <!-- お部屋選択 -->
+                    <div class="form-group pb-3">
+                        {{ Form::label('room','お部屋選択') }}
+                        {{ Form::select('room', App\Room::selectlist(), '選択してください', ['class' => 'form-control','id' => 'room']) }}
+                    </div>
+                    <!-- /お部屋選択 -->
+                    <!-- 送信ブロック -->
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            {{ Form::submit('食事プランの選択へ', ['class'=>'btn btn-primary btn-block']) }}
+                        </div>
+                    <!-- /送信ブロック -->
+                    </div>
+                {{ Form::close() }}
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
+
