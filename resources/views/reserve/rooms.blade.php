84c0f69e5b9022137fb8d6f3c77afeb3b6e9aec9
daigoron-sword <dsr.daichi@gamil.com>
2021-04-19 16:38:57 +0900

    Create rooms.blade.php

---
+++ resources/views/reserve/rooms.blade.php
@@ -0,0 +1,9 @@
+<div class="register-input-container">
+    <div class="horizontal">
+        <div>
+            <label for="register-birthday">出身地</label>
+        </div>
+        <div>
+        </div>
+    </div>
+</div>
