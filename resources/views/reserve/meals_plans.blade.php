1cbc04702afaf9507805b5ec745a8c3a065da9fc
daigoron-sword <dsr.daichi@gamil.com>
2021-04-21 22:50:30 +0900

    create view

---
+++ resources/views/reserve/meals_plans.blade.php
@@ -0,0 +1,34 @@
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<title>お食事</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-left">
+            <div class="col-md-12">
+                {{ Form::open(['url' => '/reserve/fill', 'method' => 'get', 'files' => false]) }}
+                {{ Form::token() }}
+                    {{ Session::get('date') }}
+                    <!-- お食事選択 -->
+                    <div class="form-group pb-3">
+                        {{ Form::label('room','お食事選択') }}
+                        {{ Form::select('room', App\Models\MealPlan::selectlist(), '選択してください', ['class' => 'form-control','id' => 'meal_plan']) }}
+                    </div>
+                    <!-- /お食事選択 -->
+                    <!-- 送信ブロック -->
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            {{ Form::submit('個人情報の入力へ', ['class'=>'btn btn-primary btn-block']) }}
+                        </div>
+                    <!-- /送信ブロック -->
+                    </div>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
