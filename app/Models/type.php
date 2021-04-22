dccd02b94150335e3db2631944bd34ec5dafad84
daigoron-sword <dsr.daichi@gamil.com>
2021-04-23 01:52:27 +0900

    add type_lists()

--- app/Models/type.php
+++ app/Models/type.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class type extends Model
+class Type extends Model
 {
     // 割り当てをさせないようにする
     protected $guarded = ['id'];
+
+    // typeカラムを引き出すメソッド
+    public static function type_lists()
+    {
+        $types = Type::all();
+        $type_lists = [];
+        foreach ($types as $type) 
+        {
+            $type_lists[] = $type->type;
+        }
+        return $type_lists;
+    }
 }
