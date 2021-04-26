0164426d2b968d29cec4e3760ab46fa0e34a0daf
daigoron-sword <dsr.daichi@gamil.com>
2021-04-27 03:09:07 +0900

    add function select_meal_plan_list

--- app/Models/MealPlan.php
+++ app/Models/MealPlan.php
@@ -10,17 +10,31 @@ class MealPlan extends Model
     protected $table = 'meals_plans';
     // 割り当てをさせないようにする
     protected $guarded = ['id'];
+    // format()を使うために$datesに代入
+    protected $dates = ['start_period', 'end_period'];
 
-    public static function selectlist()
+    public static function select_meal_plan_list($date)
     {
         $meals_plans = MealPlan::all();
-        $list = array();
-        $list += array("" => "選択してください");
+        // $dateをフォーマット
+        $date_f = date('Ymd',  strtotime($date));
+        $meal_plan_list = [];
+        $meal_plan_list[""] = '選択してください';
+        // 食事プランをあるだけ繰り返し
         foreach ($meals_plans as $meal_plan) 
         {
-            $list += array($meal_plan->name => $meal_plan->name);
+            // プラン開始日
+            $start_period = $meal_plan->start_period->format('Ymd');
+            // プラン終了日
+            $end_period = $meal_plan->end_period->format('Ymd');
+            // 開始日以上終了日以下の場合
+            if ($date_f >= $start_period and $date_f <= $end_period) 
+            {
+                // プランを表示する
+                $meal_plan_list[$meal_plan->name] = $meal_plan->name;
+            }
         }
-        return $list;
+        return $meal_plan_list;
     }
 
 }
