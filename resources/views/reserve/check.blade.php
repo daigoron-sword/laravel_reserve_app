de1fbf2f17a070dfb91bd768beeee2389a71e0cf
daigoron-sword <dsr.daichi@gamil.com>
2021-04-27 03:15:01 +0900

    change

--- resources/views/reserve/check.blade.php
+++ resources/views/reserve/check.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,61 +7,46 @@
     <title>check</title>
 </head>
 <body>
-  <h1>入力情報の確認</h1>
-  <form action="{{url('/reserve/thanks')}}" method='post'>
-    <dl>
-      <dt><label for="date">予約日時</label></dt>
-        <dd><p>{{ Session::get('date') }}</p></dd>
-
-      <dt><label for="name">氏名</label></dt>
-        <dd><p>{{ Session::get('name') }}</p></dd>
-
-      <dt><label for="hurigana">氏名（ふりがな）</label></dt>
-        <dd><p>{{ Session::get('hurigana') }}</p></dd>
-
-      <dt><label for="gender">性別</label></dt>
-        <dd><p>{{ Session::get('gender') }}</p></dd>
-
-      <dt><label for="email">メールアドレス</label></dt>
-        <dd><p>{{ Session::get('email') }}</p></dd>
-
-      <dt><label for="dob">生年月日</label></dt>
-        <dd><p>{{ Session::get('dob') }}</p></dd>
-
-      <dt><label for="postal">郵便番号</label></dt>
-        <dd><p>{{ Session::get('postal') }}</p></dd>
-
-      <dt><label for="prefectures">都道府県</label></dt>
-        <dd><p>{{ Session::get('prefectures') }}</p></dd>
-
-      <dt><label for="city">市区町村郡/番地</label></dt>
-        <dd><p>{{ Session::get('city') }}</p></dd>
-
-      <dt><label for="building">建物・アパート名など</label></dt>
-        <dd><p>{{ Session::get('building') }}</p></dd>
-
-      <dt><label for="tel">連絡先</label></dt>
-        <dd><p>{{ Session::get('tel') }}</p></dd>
-
-      <dt><label for="transportation">当日の交通手段</label></dt>
-        <dd><p>{{ Session::get('transportation') }}</p></dd>
-
-      <dt><label for="check_in">チェックイン予定時間</label></dt>
-        <dd><p>{{ Session::get('check_in') }}</p></dd>
-
-      <dt><label for="past">過去のご宿泊</label></dt>
-        <dd><p>{{ Session::get('past') }}</p></dd>
-
-      <dt><label for="anniversary">記念日など</label></dt>
-        <dd><p>{{ Session::get('anniversary') }}</p></dd>
-
-      <dt><label for="requests">その他ご要望など</label></dt>
-        <dd><p>{{ Session::get('requests') }}</p></dd>
-
-      <dt><label for="dinner_start_time">夕食の開始時刻</label></dt>
-        <dd><p>{{ Session::get('dinner_start_time') }}</p></dd>
-    </dl>
-    <input type="submit" value="確認">
-  </form>
+<div class="container">
+  <div class="row justify-content-left">
+    <div class="col-md-12">
+      <h1>入力情報の確認</h1>
+        {{ Form::open(['url' => '/reserve/thanks', 'method' => 'post', 'files' => false,]) }}
+        {{ Form::token() }}
+          <dl class='row'>
+            <dt class="col-md-2">予約日</dt>
+              <dd class="col-md-10"><p>{{ Session::get('date') }}</p></dd>
+            <dt class="col-md-2">お部屋</dt>
+              <dd class="col-md-10"><p>{{ Session::get('room') }}</p></dd>
+            <dt class="col-md-2">食事プラン</dt>
+              <dd class="col-md-10"><p>{{ Session::get('meal_plan') }}</p></dd>
+            <dt class="col-md-2">氏名</dt>
+              <dd class="col-md-10"><p>{{ Session::get('name') }}</p></dd>
+            <dt class="col-md-2">氏名（ふりがな）</dt>
+              <dd class="col-md-10"><p>{{ Session::get('hurigana') }}</p></dd>
+            <dt class="col-md-2">性別</dt>
+              <dd class="col-md-10"><p>{{ Session::get('gender') }}</p></dd>
+            <dt class="col-md-2">メールアドレス</dt>
+              <dd class="col-md-10"><p>{{ Session::get('mail') }}</p></dd>
+            <dt class="col-md-2">生年月日</dt>
+              <dd class="col-md-10"><p>{{ Session::get('dob') }}</p></dd>
+            <dt class="col-md-2">郵便番号</dt>
+              <dd class="col-md-10"><p>{{ Session::get('postal') }}</p></dd>
+            <dt class="col-md-2">都道府県</dt>
+              <dd class="col-md-10"><p>{{ Session::get('prefectures') }}</p></dd>
+            <dt class="col-md-2">市区町村郡/番地</dt>
+              <dd class="col-md-10"><p>{{ Session::get('city') }}</p></dd>
+            <dt class="col-md-2">建物・アパート名など</dt>
+              <dd class="col-md-10"><p>{{ Session::get('building') }}</p></dd>
+            <dt class="col-md-2">電話番号</dt>
+              <dd class="col-md-10"><p>{{ Session::get('tel') }}</p></dd>
+          </dl>
+          <!-- 利用人数 -->
+          {!! $type_view->check_render() !!}
+          {{ Form::submit('予約完了', ['class'=>'btn btn-primary btn-block']) }}
+        {{ Form::close() }}
+    </div>
+  </div>
+</div>
 </body>
 </html>
