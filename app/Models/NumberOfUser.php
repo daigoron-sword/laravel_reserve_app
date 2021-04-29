8f64fc4977360a301757413837d2dfabb4ce4c7a
daigoron-sword <dsr.daichi@gamil.com>
2021-04-29 23:51:58 +0900

    add model

---
+++ app/Models/NumberOfUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NumberOfUser extends Model
+{
+    // どのテーブルか指定する
+    protected $table = 'number_of_users';
+    // 割り当てをさせないようにする
+    protected $guarded = ['id'];
+    // 従テーブル側のリレーション
+    public function reservation()
+    {
+        return $this->belongsTo('App\Models\Reservation');
+    }
+    public function type()
+    {
+        return $this->belongsTo('App\Models\Type');
+    }
+
+
+}
