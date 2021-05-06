1fb7088b2152324082f2365129028f3d21a68efd
daigoron-sword <dsr.daichi@gamil.com>
2021-05-06 23:14:37 +0900

    create FormRequest

---
+++ app/Http/Requests/ReserveRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReserveRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->path() == 'reserve/fill')
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'hurigana' => 'required',
+            'gender' => 'required',
+            'mail' => 'required',
+            'dob' => 'required',
+            'postal' => 'required',
+            'prefectures' => 'required',
+            'city' => 'required',
+            'building' => 'required',
+            'tel' => 'required',
+            'transportation' => 'required',
+            'request' => 'required',
+            'dinner_start_time' => 'required'
+        ];
+    }
+}
