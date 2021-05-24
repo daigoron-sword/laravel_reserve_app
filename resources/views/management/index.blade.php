4b84f50dcd7bd89f78cb96e8be16b49828a9a5c1
daigoron-sword <dsr.daichi@gamil.com>
2021-05-24 18:27:03 +0900

    create

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/calendar.css') }}" rel="stylesheet">
+    <title>予約</title>
+</head>
+<body>
+    <p>test</p>
+</body>
+</html>
