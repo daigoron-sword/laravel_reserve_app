e9be0f86cca1864247559c5f78e8a7ce76e9a801
daigoron-sword <dsr.daichi@gamil.com>
2021-05-29 16:28:54 +0900

    add getSumPrice, getSumUsers

--- app/MyClasses/Management/ManagementView.php
+++ app/MyClasses/Management/ManagementView.php
@@ -2,6 +2,8 @@
 namespace App\Myclasses\Management;
 
 use App\Models\Reservation;
+use App\Models\NumberOfUser;
+
 
 class ManagementView
 {
@@ -27,7 +29,7 @@ class ManagementView
 		$html[] = '</thead>';
         $html[] = '<tbody　class="table table-striped">';		
 
-        $results = Reservation::with(['mealPlan', 'room'])->get();
+        $reservations = Reservation::with(['mealPlan', 'room'])->get();
         foreach($reservations as $reservation)
         {
             $html[] = '<tr>';
@@ -35,7 +37,7 @@ class ManagementView
             $html[] = $reservation->reserved_on; //予約日
             $html[] = '</td>';
             $html[] = '<td>';
-            $html[] = ''; //利用人数
+            $html[] = $this->getSumUsers($reservation); //利用人数
             $html[] = '</td>';
             $html[] = '<td>';
             $html[] = $reservation->customer->name; //代表者名
@@ -47,7 +49,7 @@ class ManagementView
             $html[] = $reservation->room->name; //部屋
             $html[] = '</td>';
             $html[] = '<td>';
-            $html[] = ''; //合計金額
+            $html[] = $this->getSumPrice($reservation); //合計金額
             $html[] = '</td>';
             $html[] = '</tr>';
         }
@@ -58,9 +60,48 @@ class ManagementView
         // 何も入れない文字列連結
         return implode("", $html);
     }
-
-    protected function getSum()
+    /**
+     * 合計金額の計算
+     */
+    protected function getSumPrice($reservation)
     {
+        $number_of_users = NumberOfUser::with('type')->where('reserve_id', $reservation->id)->get();
+        foreach($number_of_users as $number_of_user)
+        {
+           //合計金額の計算
+           if($number_of_user->type_id == 1 || $number_of_user->type_id == 2 || $number_of_user->type_id == 3) //もしtype_idが1,2,3なら
+            {
+                if($number_of_user->type_id == 1 || $number_of_user->type_id == 2 ) //もしtype_idが1,2なら
+                {
+                    $price[] = ($reservation->room->price + $reservation->mealPlan->price) * $number_of_user->number_of_person; //（部屋の値段＋食事プランの値段）×人数
+                }
+                if($number_of_user->type_id == 3)//もしtype_idが３なら
+                {
+                    $price[] = ($reservation->room->price + $reservation->mealPlan->price) * $number_of_user->numeber_of_person - ($number_of_user->type->price * $number_of_user->number_of_person); //（部屋の値段＋食事プランの値段）* 人数 - （typeのprice * 人数）
+                }
+            
+            } else //もしtype_idが1,2,3以外なら
+            {
+                $price[] = $number_of_user->type->price * $number_of_user->number_of_person; //人数 * typeの値段
+            }
+        }
+        $sum = array_sum($price);// 配列の中の数値を合計
+        return $sum . '円';
+    }
+    /**
+     * 合計人数の計算
+     */
+    protected function getSumUsers($reservation)
+    {
+        $number_of_users = NumberOfUser::where('reserve_id', $reservation->id)->get();
+        foreach($number_of_users as $number_of_user)
+        {
+            $user[] = $number_of_user->number_of_person;
+        }
+        $sum = array_sum($user);
+        return $sum;
+  
 
     }
+
 }
