992477c3fa5828c334fdd7ed26dfc7ce7a1c063f
daigoron-sword <dsr.daichi@gamil.com>
2021-05-30 08:29:19 +0900

    add editRoom, finishRoom

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -9,7 +9,7 @@ use App\Models\Reservation;
 
 class ManagementController extends Controller
 {
-   public function reserve_management(Request $request)
+   public function reserveManagement(Request $request)
    {
       $management = new ManagementView();
       return view('management.index', ['management' => $management]);
@@ -17,8 +17,24 @@ class ManagementController extends Controller
 
    public function edit(Request $request)
    {
-      $reservation = Reservation::with('room')->find($request->id);
-      return dump($reservation);
-      return view('edit');
+      $reservation = Reservation::with('customer')->find($request->id);
+      return view('management.edit', ['reservation' => $reservation]);
+   }
+
+   public function editRoom(Request $request)
+   {
+      $reservation = Reservation::find($request->id);
+      return view('management.editRoom', ['reservation' => $reservation]);
+   }
+
+   public function finishRoom(Request $request)
+   {
+      $validate_rule =['room_id' => 'required']; //；部屋選択のバリデーションルール
+      $this->validate($request, $validate_rule);
+      $reservation = Reservation::find($request->id);
+      $reservation->room_id = $request->room_id;
+      $reservation->save();
+      // return redirect()->route('editRoom', ['reservation' => $reservation]);
+      return view('management.editRoom', ['reservation' => $reservation]);
    }
 }
