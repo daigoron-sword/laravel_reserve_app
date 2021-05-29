7b93789aa6cdb77ef31104a1fbc2e1e013ee1883
daigoron-sword <dsr.daichi@gamil.com>
2021-05-29 21:18:11 +0900

    add edit

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -4,32 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Myclasses\Management\ManagementView;
-
-
-/**
- * 試験
- */
-use App\Models\NumberOfUser;
 use App\Models\Reservation;
-
 
 
 class ManagementController extends Controller
 {
    public function reserve_management(Request $request)
    {
-      /**
-       * 試験
-      */
-      // $number_of_users = NumberOfUser::where('reserve_id', 5)->get();
-      // foreach($number_of_users as $number_of_user)
-      // {
-      //    $user[] = $number_of_user->number_of_person;
-      // }
-      // $sum = array_sum($user);
-      // return dump($sum);
-
       $management = new ManagementView();
       return view('management.index', ['management' => $management]);
+   }
+
+   public function edit(Request $request)
+   {
+      $reservation = Reservation::with('room')->find($request->id);
+      return dump($reservation);
+      return view('edit');
    }
 }
