326380e52c07a61fc88e148ca05b501786dca4c6
daigoron-sword <dsr.daichi@gamil.com>
2021-05-29 16:29:06 +0900

    fix

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -21,28 +21,13 @@ class ManagementController extends Controller
       /**
        * 試験
       */
-      $reservation = Reservation::with(['mealPlan', 'room'])->find(3);
-
-      $results = NumberOfUser::with('type')->where('reserve_id', 3)->get();
-      foreach($results as $result)
-      {
-         //合計金額の計算
-         if($result->type_id == 1 || $result->type_id == 2 || $result->type_id == 3) //もしtype_idが1,2,3なら
-         {
-            if($result->type_id == 1 || $result->type_id == 2 ) //もしtype_idが1,2なら
-            {
-               $sum[] = ($reservation->room->price + $reservation->mealPlan->price) * $result->number_of_person; //（部屋の値段＋食事プランの値段）×人数
-            }
-            //もしtype_idが３なら
-            //（部屋の値段＋食事プランの値段）* 人数 - （typeのprice * 人数）
-         
-         } else{ //もしtype_idが1,2,3以外なら
-         //人数 * typeの値段
-         }
-         
-         
-      }
-
+      // $number_of_users = NumberOfUser::where('reserve_id', 5)->get();
+      // foreach($number_of_users as $number_of_user)
+      // {
+      //    $user[] = $number_of_user->number_of_person;
+      // }
+      // $sum = array_sum($user);
+      // return dump($sum);
 
       $management = new ManagementView();
       return view('management.index', ['management' => $management]);
