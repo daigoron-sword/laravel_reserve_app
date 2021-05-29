5e97fa8fc7053479111021d8b9368ff3b102a9b0
daigoron-sword <dsr.daichi@gamil.com>
2021-05-29 21:17:23 +0900

    create edit.blade.php

---
+++ resources/views/management/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/calendar.css') }}" rel="stylesheet">
+    <title>編集</title>
+</head>
+<body>
+    <h1>編集画面</h1>
+</body>
+</html>
