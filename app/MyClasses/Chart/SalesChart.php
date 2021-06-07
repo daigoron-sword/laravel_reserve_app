a5b21123bf8c390cd40a3eb4736de29a3d8c3a17
daigoron-sword <dsr.daichi@gamil.com>
2021-06-07 09:10:37 +0900

    add method

--- app/MyClasses/Chart/SalesChart.php
+++ app/MyClasses/Chart/SalesChart.php
@@ -15,16 +15,6 @@ class SalesChart
         $this->carbon = new Carbon($date);
     }
 
-    public function getSalesChartLogData($date)
-    {
-        $logs = Reservation::where('reserved_on', 'like', $date . '%')->get();
-            foreach($logs as $log)
-            {
-                $sale = $log->sum;
-            }
-        if(!isset($sale)) $sale = 0;
-        return $sale;
-    }
 
     /**
      * ここから試験運用
@@ -34,19 +24,53 @@ class SalesChart
      */
     public function getNextMonth()
     {
-        return $this->carvon->copy()->addMonthsNoOverflow()->format('Y-m');
+        return $this->carbon->copy()->addMonthsNoOverflow()->format('Y-m');
+    }
+    // /**
+    //  * 前の月
+    //  */
+    public function getPreviousMonth()
+    {
+        return $this->carbon->copy()->subMonthsNoOverflow()->format('Y-m');
     }
     /**
-     * 前の月
+     * 過去1年分の年月ラベルの取得
      */
-    public function getPrevioustMonth()
+    public function getLabels()
     {
-        return $this->carvon->copy()->subMonthsNoOverflow()->format('Y-m');
+        for ($i = 1; $i <= 12; $i++) //取得した日付のひと月前から過去1年分の年月を繰り返す
+        {
+            $lavels[] = $this->carbon->copy()->subMonthsNoOverflow($i)->format('Y年m月');
+        }
+        sort($lavels);
+        return $lavels;
+    }
+    /**
+     * 過去1年分のデータを取得
+     */
+    public function getSalesChartLogData()
+    {
+        for ($i = 1; $i <= 12; $i++) //取得した日付のひと月前から過去1年分の年月を繰り返す
+        {
+            $target_days[] = $this->carbon->copy()->subMonthsNoOverflow($i)->format('Y-m');
+        }
+        sort($target_days);
+        foreach($target_days as $day)
+        {
+            $sale_log[] = $this->getSaleLogData($day);
+        }
+        return $sale_log;
     }
 
-    public function getLabel()
+    protected function getSaleLogData($date)
     {
-        
+        $logs = Reservation::where('reserved_on', 'like', $date . '%')->get();
+            foreach($logs as $log)
+            {
+                $sale = $log->sum;
+            }
+        if(!isset($sale)) $sale = 0;
+        return $sale;
     }
 
 }
