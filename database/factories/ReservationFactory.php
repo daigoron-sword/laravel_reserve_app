0aed5ee6cf97524b963b03d4d334d6f68ee9fdce
daigoron-sword <dsr.daichi@gamil.com>
2021-06-12 21:29:22 +0900

    Create factory
    ファクトリーを追加。NumberOfUserだけ問題あり。

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Reservation::class, function (Faker $faker) {
+    return [
+        'customer_id' => factory(App\Models\Customer::class)->create(),
+        'room_id' => $faker->numberBetween(1,4),
+        'meal_plan_id' => $faker->numberBetween(1,4),
+        'reserved_on' => $faker->dateTimeBetween('-1 years', 'now', date_default_timezone_get()),
+        'transportation' => '車',
+        'check_in_time' => '16:00:00',
+        'request' => $faker->realText(50, 2),
+        'dinner_start_time' => '17:00:00',
+        'sum' => $faker->numberBetween(20000, 50000),
+        'created_at' => $faker->date('Y-m-d H:i:s', 'now'),
+        'updated_at' => $faker->date('Y-m-d H:i:s', 'now'),
+    ];
+});
