3abb79936efc65736c1429049b13c2be48b86b10
daigoron-sword <dsr.daichi@gamil.com>
2021-06-19 21:26:41 +0900

    Fix
    関係のないlinkタグを修正。

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/calendar.css') }}" rel="stylesheet">
     <title>予約管理</title>
 </head>
 <body>
