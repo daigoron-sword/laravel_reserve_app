21b1c0218faa3f9ae9b8631ae48fe1c819db4d5e
daigoron-sword <dsr.daichi@gamil.com>
2021-06-20 22:05:14 +0900

    Fix param
    キャパシティデータを削除し、新たに適用期間データを追加。

--- database/seeds/RoomsTableSeeder.php
+++ database/seeds/RoomsTableSeeder.php
@@ -16,28 +16,33 @@ class RoomsTableSeeder extends Seeder
         $param = [
             'name' => 'room_a',
             'price' => '5000',
-            'capacity' => '2'
+            'start_period' => '20210601',
+            'end_period' => '20221201',
+            
         ];
         DB::table('rooms')->insert($param);
 
         $param = [
             'name' => 'room_b',
             'price' => '10000',
-            'capacity' => '2'
+            'start_period' => '20210601',
+            'end_period' => '20221201',
         ];
         DB::table('rooms')->insert($param);
 
         $param = [
             'name' => 'room_c',
             'price' => '15000',
-            'capacity' => '3'
+            'start_period' => '20210601',
+            'end_period' => '20221201',
         ];
         DB::table('rooms')->insert($param);
 
         $param = [
             'name' => 'room_d',
             'price' => '20000',
-            'capacity' => '4'
+            'start_period' => '20210601',
+            'end_period' => '20221201',
         ];
         DB::table('rooms')->insert($param);
 
