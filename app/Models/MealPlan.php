0aefb2f3ad1bcc673cb4a98cd1220361afff6858
daigoron-sword <dsr.daichi@gamil.com>
2021-06-23 19:03:25 +0900

    Fix select_meal_plan_list
    冗長を修正。

--- app/Models/MealPlan.php
+++ app/Models/MealPlan.php
@@ -15,24 +15,13 @@ class MealPlan extends Model
 
     public static function select_meal_plan_list($date)
     {
-        $meals_plans = MealPlan::all();
-        // $dateをフォーマット
-        $date_f = date('Ymd',  strtotime($date));
+        $meal_plans = MealPlan::where('start_period', '<=', $date)->where('end_period', '>=', $date)->get();
         $meal_plan_list = [];
         $meal_plan_list[""] = '選択してください';
-        // 食事プランをあるだけ繰り返し
-        foreach ($meals_plans as $meal_plan) 
+        
+        foreach ($meal_plans as $meal_plan) 
         {
-            // プラン開始日
-            $start_period = $meal_plan->start_period->format('Ymd');
-            // プラン終了日
-            $end_period = $meal_plan->end_period->format('Ymd');
-            // 開始日以上終了日以下の場合
-            if ($date_f >= $start_period and $date_f <= $end_period) 
-            {
-                // プランを表示する
-                $meal_plan_list[$meal_plan->id] = $meal_plan->name;
-            }
+            $meal_plan_list[$meal_plan->id] = $meal_plan->name;
         }
         return $meal_plan_list;
     }
