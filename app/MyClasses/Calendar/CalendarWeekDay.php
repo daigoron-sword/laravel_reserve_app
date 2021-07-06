f04c72ece1ee913d4084ec35ecd6679cd9b339a0
daigoron-sword <dsr.daichi@gamil.com>
2021-07-06 13:43:24 +0900

    Delete encrypt
    仕様変更によりencrypt処理を削除

--- app/MyClasses/Calendar/CalendarWeekDay.php
+++ app/MyClasses/Calendar/CalendarWeekDay.php
@@ -27,7 +27,6 @@ class CalendarWeekDay {
 	function reservedOn() //残りの部屋数を出力
     {
         $date = $this->carbon->copy()->format('Y-m-d'); //カレンダーの日付
-        $date_encrypt = encrypt($date); // aタグ用に暗号化させる
         //適用期間中の部屋数をカウント
         $room_c = Room::where('start_period', '<=', $date)->where('end_period', '>=', $date)->count();
 
@@ -49,7 +48,7 @@ class CalendarWeekDay {
         $reserved_on_c = Reservation::where('reserved_on', $date)->count();// その日の予約数
         $room_remaining = $room_c - $reserved_on_c; //残りの部屋数
         if($room_remaining == 0) return '満室';
-        return '<a href="'.route('reserve.rooms').'?date=' . $date_encrypt . ' ">残り' . $room_remaining . '部屋</a>' ;
+        return '<a href="'.route('reserve.rooms').'?date=' . $date . ' ">残り' . $room_remaining . '部屋</a>' ;
         // return '<a href="/reserve/rooms?date=' . $date_encrypt . ' ">残り' . $room_remaining . '部屋</a>' ;
     }
 }	
