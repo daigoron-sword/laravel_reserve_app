9cc79b90e45b34f1d47a3ca46700a1a9c63e7044
daigoron-sword <dsr.daichi@gamil.com>
2021-07-18 11:43:31 +0900

    マネジメントコントローラーテストの作成

---
+++ tests/Feature/ManagementControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManagementControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testIndex()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get(route('management'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('management.index');
+    }
+}
