5fcce667cf624f17eb28aaf6052339d40b37550f
daigoron-sword <dsr.daichi@gamil.com>
2021-07-22 18:53:13 +0900

    タイプミスの修正

--- app/Mail/ConfirmMail.php
+++ app/Mail/ConfirmMail.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Myclasses\Type\TypeView;
+use App\MyClasses\Type\TypeView;
 
 class ConfirmMail extends Mailable
 {
